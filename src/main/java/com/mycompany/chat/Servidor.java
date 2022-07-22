486eb096c44131bf321cc64cfe1d87ba2723ede4
Juan David Garces Leon <juan.garces.leon@correounivalle.edu.co>
2022-07-22 17:50:07 -0500

    server message

--- src/main/java/com/mycompany/chat/Servidor.java
+++ src/main/java/com/mycompany/chat/Servidor.java
@@ -4,11 +4,14 @@
  */
 package com.mycompany.chat;
 
+import java.awt.event.*;
 import javax.swing.*;
 import java.awt.*;
 import java.io.*;
 import java.net.*;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -21,65 +24,90 @@ public class Servidor {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+
         MarcoServidor miMarco = new MarcoServidor();
         miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
     }
-    
+
 }
 
 
 class MarcoServidor extends JFrame implements Runnable{
-    
+
     public MarcoServidor(){
-        
+
         setBounds(1200, 300, 280, 350);
-        
+
         JPanel miLamina = new JPanel();
-        
+
         miLamina.setLayout(new BorderLayout());
-        
+
         areaTexto= new JTextArea();
-        
+
         miLamina.add(areaTexto, BorderLayout.CENTER);
-        
+
         add(miLamina);
-        
+
         setVisible(true);
-        
+
         Thread miHilo = new Thread(this);
-        
+
         miHilo.start();
     }
-    
+
 
     @Override
     public void run() {
+
+
         try {
             //System.out.println("Estoy a la escucha");
 
             ServerSocket servidor = new ServerSocket(9999);
-            
+
+            String nick, ip, mensaje;
+
+            InformacionEnvio informacionRecibida;
+
             while(true){
                 Socket miSocket = servidor.accept();
-            
-                DataInputStream flujoEntrada = new DataInputStream(
+
+                ObjectInputStream paqueteDatos = new ObjectInputStream(miSocket.getInputStream());
+
+                informacionRecibida = (InformacionEnvio)paqueteDatos.readObject();
+
+                nick = informacionRecibida.getNick();
+                ip = informacionRecibida.getIp();
+                mensaje = informacionRecibida.getMensaje();
+
+                /*DataInputStream flujoEntrada = new DataInputStream(
                     miSocket.getInputStream());
-            
+
                 String mensajeTexto = flujoEntrada.readUTF();
-            
-                areaTexto.append("\n" + mensajeTexto);
-            
-                miSocket.close();  
+
+                areaTexto.append("\n" + mensajeTexto);*/
+
+                areaTexto.append("\n" + nick + ": " + mensaje + " para " + ip);
+
+                Socket enviaDestinatario = new Socket(ip, 9090);
+
+                ObjectOutputStream informacionReenvio = new ObjectOutputStream(
+                        enviaDestinatario.getOutputStream());
+
+                informacionReenvio.writeObject(informacionRecibida);
+
+                enviaDestinatario.close();
+
+
+                miSocket.close();
             }
-            
-        } catch (IOException ex) {
-            
+
+        } catch (IOException | ClassNotFoundException ex) {
             ex.printStackTrace();
         }
-                   
+
     }
-    
+
     private JTextArea areaTexto;
 }
